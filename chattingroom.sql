22dfd139b06d1f0768aea778acfed1b6de356510
MainGuy <459789143@qq.com>
2019-08-01 16:00:41 +0800

    修复了分组内好友的编辑删除按钮bug 增加修改好友备注的功能

--- chattingroom.sql
+++ chattingroom.sql
@@ -10,10 +10,30 @@ Target Server Type    : MYSQL
 Target Server Version : 80013
 File Encoding         : 65001
 
-Date: 2019-07-03 10:03:54
+Date: 2019-07-31 18:48:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `friend`
+-- ----------------------------
+DROP TABLE IF EXISTS `friend`;
+CREATE TABLE `friend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `list_id` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `friend_id` varchar(32) DEFAULT NULL,
+  `friend_remark` varchar(32) DEFAULT NULL,
+  `add_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of friend
+-- ----------------------------
+INSERT INTO `friend` VALUES ('3', 'FDE2CEE7C2604A5299722523E64CA8AD', '1', 'C11678D5D6634B62A59AB12E792EE597', null, null);
+INSERT INTO `friend` VALUES ('4', 'FDE2CEE7C2604A5299722523E64CA8AD', '0', '7A088509B77D43688970E147243D1C71', null, null);
 
 -- ----------------------------
 -- Table structure for `friend_list`
@@ -22,32 +42,18 @@ DROP TABLE IF EXISTS `friend_list`;
 CREATE TABLE `friend_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(32) DEFAULT NULL,
-  `list_id` varchar(32) DEFAULT NULL,
+  `list_id` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `list_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of friend_list
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `friend_list_content`
--- ----------------------------
-DROP TABLE IF EXISTS `friend_list_content`;
-CREATE TABLE `friend_list_content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `list_id` varchar(32) DEFAULT NULL,
-  `friend_id` varchar(32) DEFAULT NULL,
-  `friend_remark` varchar(32) DEFAULT NULL,
-  `add_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of friend_list_content
--- ----------------------------
+INSERT INTO `friend_list` VALUES ('3', 'C56CDA7AB8A146D9ABB353BEF2A0A8DF', '0', '我的好友');
+INSERT INTO `friend_list` VALUES ('4', 'FDE2CEE7C2604A5299722523E64CA8AD', '0', '我的好友');
+INSERT INTO `friend_list` VALUES ('5', 'FDE2CEE7C2604A5299722523E64CA8AD', '1', '亲友团');
+INSERT INTO `friend_list` VALUES ('6', '7A088509B77D43688970E147243D1C71', '0', '我的好友');
 
 -- ----------------------------
 -- Table structure for `friend_message_record`
@@ -131,7 +137,7 @@ CREATE TABLE `user` (
   `nickname` varchar(10) DEFAULT NULL,
   `password` varchar(16) DEFAULT NULL,
   `telephone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `sex` int(2) DEFAULT NULL,
+  `sex` varchar(2) DEFAULT NULL,
   `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
@@ -144,4 +150,6 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('D4A0E9F0E5D741B4A7F18D09D08A3D72', 'admin', 'admin', 'admin', '15888888888', '0', 'admin', '1995-06-15', '2019-07-02 17:15:43', '2019-07-02 17:56:14', '2019-07-02 19:11:30', null);
+INSERT INTO `user` VALUES ('7A088509B77D43688970E147243D1C71', 'test2018', 'test2018', 'test2018', '15812345678', '2', 'test2018', '2019-07-17', '2019-07-25 16:30:18', null, '2019-07-25 16:33:08', null);
+INSERT INTO `user` VALUES ('C11678D5D6634B62A59AB12E792EE597', 'admin', 'admin', 'admin', '15812345678', '2', 'admin', '2019-07-03', '2019-07-17 14:54:37', null, '2019-07-25 16:32:37', null);
+INSERT INTO `user` VALUES ('FDE2CEE7C2604A5299722523E64CA8AD', 'mainguy', 'mainguy', '123456', '5456456464', '2', 'test2018', '2019-07-03', '2019-07-17 14:59:13', '2019-07-29 15:25:06', '2019-07-31 17:06:50', '\\head_icon_img\\mainguy.jpg');
